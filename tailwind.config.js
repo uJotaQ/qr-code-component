a0159bc549285c937c1ad4e2882dfba649fe7b42
uJotaQ <joao.gabrielss15253@gmail.com>
2024-09-28 16:16:34 -0300

    Finalizando site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './index.html', // Adicione o caminho do seu arquivo HTML
+    './styles.css'  // Adicione o caminho do seu arquivo CSS, se necessário
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
